d1954cc205b72264001cae6c0bdd2f9760b54153
a.rogov <a.rogov@intelprime.com>
2019-06-05 12:37:33 +0300

    Base Added

---
+++ garakhTest/garakhTest/Context/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using garakhTest.Entities;
+using garakhTest.Interfaces;
+
+namespace garakhTest.Context
+{
+    public class UserRepository : IRepository<User>
+    {
+        public void Dispose()
+        {
+
+        }
+
+        public IEnumerable<User> ReadList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
