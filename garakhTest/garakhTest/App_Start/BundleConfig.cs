e02556759048a31b99c297df608020ddee9e390e
a.rogov <a.rogov@intelprime.com>
2019-06-06 17:55:22 +0300

    Done and tasted

--- garakhTest/garakhTest/App_Start/BundleConfig.cs
+++ garakhTest/garakhTest/App_Start/BundleConfig.cs
@@ -11,6 +11,7 @@ namespace garakhTest
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery.bootgrid.js",
+                        "~/Scripts/jquery-ui.js",
                         "~/Scripts/app.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
@@ -24,7 +25,8 @@ namespace garakhTest
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/jquery.bootgrid.css"));
+                      "~/Content/jquery.bootgrid.css",
+                      "~/Content/jquery-ui.css"));
         }
     }
 }
--- garakhTest/garakhTest/Scripts/app.js
+++ garakhTest/garakhTest/Scripts/app.js
@@ -1,5 +1,4 @@
 ﻿$(document).ready(function () {
-
 var grid = $("#grid-data").bootgrid({
     ajax: true,
     ajaxSettings: {
@@ -16,10 +15,12 @@ var grid = $("#grid-data").bootgrid({
     }
 }).on("loaded.rs.jquery.bootgrid", function () {
     /* Executes after data is loaded and rendered */
-    grid.find(".command-edit").on("click", function (e) {
-        alert("You pressed edit on row: " + $(this).data("row-id"));
+    grid.find(".command-edit").on("click", function (e) {        
+        $("#dialog-form")
+            .data("itemId", $(this).data("row-id"))  // Set your data values
+            .dialog("open");
+
     }).end().find(".command-delete").on("click", function (e) {
-        var a = window.location.origin + '/api/values/' + $(this).data("row-id");
         $.ajax({
             url: window.location.origin + '/api/values/' + $(this).data("row-id"),
             type: 'DELETE',
@@ -28,7 +29,75 @@ var grid = $("#grid-data").bootgrid({
             }
         });
     });
-});
+        });
+
+    var modalData = undefined;
+    $("#dialog-form").dialog({
+        resizable: false,
+        height: 300,
+        width: 400,
+        modal: true,
+        autoOpen: false,
+        beforeClose: function () {
+            $(this).data("isNew", false)
+            $("#modalName").val("");
+            $("#modalGroup").val("");
+        },
+        open: function () {
+            modalData = undefined;
+            if ($(this).data("isNew") != true) {
+                $.get(window.location.origin + '/api/values/' + $(this).data("itemId"), function (data, status) {
+                if (status == "success") {
+                    modalData = data;
+                    $("#modalName").val(data.Name);
+                    $("#modalGroup").val(data.Group);
+                    console.log(modalData);
+                };
+            })
+            }
+            
+        },
+        buttons: {
+            "OK": function () {
+                if (modalData != undefined) {
+                    modalData.Name = $("#modalName").val();
+                    modalData.Group = $("#modalGroup").val();
+                    $.ajax({
+                        url: window.location.origin + '/api/values/' + $(this).data("itemId"),
+                        type: 'PUT',
+                        success: function () {
+                            $("#grid-data").bootgrid("reload");
+                        },
+                        data: modalData,
+                    });
+                } 
+                if ($(this).data("isNew") == true) {
+                    modalData = { Id: 0, Name: $("#modalName").val(), Group: $("#modalGroup").val() };
+                    $.ajax({
+                        url: window.location.origin + '/api/values',
+                        type: 'POST',
+                        success: function () {
+                            $("#grid-data").bootgrid("reload");
+                        },
+                        data: modalData,
+                    });
+
+                }
+
+                $(this).dialog("close");             
+            },
+            "Cancel": function () {
+                $(this).dialog("close");
+            }
+        }
+    });
+
+    $("#addBtn").click(function () {
+        modalData = undefined;
+        $("#dialog-form")
+            .data("isNew", true)  
+            .dialog("open");
+    })
 
 });
 
